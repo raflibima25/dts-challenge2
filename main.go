0ad615f89c59e60c8b3078ee879ef325892aada1
Rafli Bima Pratandra <raflibima1106@gmail.com>
2023-04-10 10:34:16 +0700

    challenge 2

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	for i := 0; i <= 4; i++ {
+		fmt.Println("Nilai i = ", i)
+	}
+
+	for j := 0; j <= 10; j++ {
+		if j == 5 {
+			str := "СAАAШLАCРNВOО"
+			for i, v := range str {
+				if i%2 == 1 {
+					continue
+				}
+				fmt.Printf("character %#U starts at byte position %d \n", v, i)
+			}
+			continue
+		}
+		fmt.Println("Nilai j = ", j)
+	}
+
+	// another ver
+	/*
+		for j := 0; j <= 10: j++ {
+			if j == 5 {
+				teks := []rune('С','А','Ш','А','Р','В','О')
+				for indeks := 0; indeks < len(teks); indeks++ {
+					char := teks[indeks]
+					fmt.Printf("character %U '%s' start at byte position %d \n", char, string(char), indeks*2)
+				}
+			} else {
+				fmt.Println("Nilai j = ", j)
+			}
+		}
+
+	*/
+}
